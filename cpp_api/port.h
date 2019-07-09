bb44a2e37613b499ef065cef1165fef95bf398c7
wuyuanyi <wuyuanyi135@gmail.com>
2019-07-09 11:47:14 -0400

    port access macros

--- cpp_api/port.h
+++ cpp_api/port.h
@@ -11,6 +11,22 @@
 #define BLOCK_REGISTER_INPUT_PORT(BLK, PORT_ID) BLK->inputPorts.push_back(std::make_shared<RuntimeInputPort>(*BLK, PORT_ID))
 #define BLOCK_REGISTER_OUTPUT_PORT(BLK, PORT_ID) BLK->outputPorts.push_back(std::make_shared<RuntimeOutputPort>(*BLK, PORT_ID))
 
+// Access the void* pointer of the output port
+#define OUTPUTPORT_POINTER(OUTPUTPORT) (OUTPUTPORT->data)
+
+// Access the void* pointer of the input port
+#define INPUTPORT_POINTER(INPUTPORT) (INPUTPORT->data)
+
+// Access the pointer with type cast short hand.
+#define OUTPUTPORT_TYPED_POINTER(OUTPUTPORT, T) ((T)OUTPUTPORT_POINTER(OUTPUTPORT))
+
+// Access the pointer with type cast short hand.
+#define INPUTPORT_TYPED_POINTER(INPUTPORT, T) ((T)INPUTPORT_POINTER(INPUTPORT))
+
+// Access the pointed memory (*(T)ptr) with type cast short hand.
+#define OUTPUTPORT_TARGET(OUTPUTPORT, T) (* OUTPUTPORT_TYPED_POINTER(OUTPUTPORT, T))
+#define INPUTPORT_TARGET(INPUTPORT, T) (* INPUTPORT_TYPED_POINTER(INPUTPORT, T))
+
 class Block;
 class Port {
 public:
--- test/SimpleExample/SimpleExample.cpp
+++ test/SimpleExample/SimpleExample.cpp
@@ -11,49 +11,49 @@
 
 BlockMeta::BlockMeta(SimStruct &S) {
 
-    DEBUGV_LIFECYCLE_PRINTF("BlockMeta Initialized (SimpleExample)\n");
+  DEBUGV_LIFECYCLE_PRINTF("BlockMeta Initialized (SimpleExample)\n");
 
-    registerInputPort(SS_DOUBLE, 1, COMPLEX_NO, FRAME_NO, true);
-    registerInputPort(SS_DOUBLE, 1, COMPLEX_NO, FRAME_NO, true);
-    registerOutputPort(SS_DOUBLE, 1, COMPLEX_NO, FRAME_NO);
+  registerInputPort(SS_DOUBLE, 1, COMPLEX_NO, FRAME_NO, true);
+  registerInputPort(SS_DOUBLE, 1, COMPLEX_NO, FRAME_NO, true);
+  registerOutputPort(SS_DOUBLE, 1, COMPLEX_NO, FRAME_NO);
 
-    SampleTime st;
-    st.sampleTime = 1;
-    st.offset = 0;
-    sampleTime.push_back(st);
+  SampleTime st;
+  st.sampleTime = 1;
+  st.offset = 0;
+  sampleTime.push_back(st);
 }
 
 BlockMeta::~BlockMeta() {
-    DEBUGV_LIFECYCLE_PRINTF("BlockMeta destroyed\n");
+  DEBUGV_LIFECYCLE_PRINTF("BlockMeta destroyed\n");
 }
-
 
 /// Block
 class SimpleExampleBlock : public Block {
-public:
-    SimpleExampleBlock(SimStruct &S) : Block(S) {
+ public:
+  SimpleExampleBlock(SimStruct &S) : Block(S) {
 
-    }
+  }
 
-    void onStart() override {
-    }
+  void onStart() override {
+  }
 
-    void onTerminate() override {
-    }
+  void onTerminate() override {
+  }
 
-    void onUpdate() override {
-        (* (real64_T*) (outputPorts[0]->data)) = *(real64_T *) inputPorts[0]->data + *(real64_T *) inputPorts[1]->data;
-    }
+  void onUpdate() override {
+    // Expansion:         (* (real64_T*) (outputPorts[0]->data)) = *(real64_T *) inputPorts[0]->data + *(real64_T *) inputPorts[1]->data;
+    OUTPUTPORT_TARGET(outputPorts[0], real64_T*) =
+        INPUTPORT_TARGET(inputPorts[0], real64_T*) + INPUTPORT_TARGET(inputPorts[1], real64_T*);
+  }
 
-    void onOutput() override {
-    }
+  void onOutput() override {
+  }
 };
 
+std::shared_ptr<Block> createBlock(SimStruct *S) {
 
-std::shared_ptr<Block> createBlock(SimStruct* S) {
-
-    return std::make_shared<SimpleExampleBlock>(*S);
+  return std::make_shared<SimpleExampleBlock>(*S);
 }
-std::shared_ptr<BlockMeta> createBlockMeta(SimStruct* S) {
-    return std::make_shared<BlockMeta>(*S);
+std::shared_ptr<BlockMeta> createBlockMeta(SimStruct *S) {
+  return std::make_shared<BlockMeta>(*S);
 }
