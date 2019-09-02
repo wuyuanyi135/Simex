c400023031c11e1893207ea8ddd4c439c9ca358f
Yuanyi Wu <wuyuanyi135@gmail.com>
2019-09-02 16:04:11 -0400

    Remove unnecessary tests

--- test/DynamicMaskDisplayExample/DynamicMaskDisplayExampleTestMain.m
+++
@@ -1,4 +0,0 @@
-function DynamicMaskDisplayExampleTestMain()
-% TODO
-
-end
--- test/DynamicMaskDisplayExample/disp_listener.m
+++
@@ -1,4 +0,0 @@
-function disp_listener(s,e)
-set_param(get_param(s.BlockHandle, 'Parent'),  'MaskDisplay', sprintf('disp("Value=%f");',s.InputPort(1).Data));
-end
-
